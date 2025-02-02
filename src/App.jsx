1d9f82f19a9495c015354e292393592540e2bf88
이종훈 <aljongjong@gmail.com>
2025-02-02 15:50:40 +0900

    day 12 미션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Welcome from "./components/day10/Welcome";
-import OrderEditor from "./components/day11/OrderEditor";
+import OrderEditor from "./components/day12/OrderEditor";
 
 function App() {
   /* day10
@@ -17,7 +17,13 @@ function App() {
 
   // --------------------------------------------------
 
-  /* day 11 */
+  /* day 11
+  return <OrderEditor />;
+   */
+
+  // --------------------------------------------------
+
+  /* day 12 */
   return <OrderEditor />;
 }
 
